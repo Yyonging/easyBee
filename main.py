fe42b12d02fd1eb1275d94a03c5e35ae1e59a4c3
Yyonging <sysuduanyongqiang@163.com>
2020-11-12 17:32:42 +0800

    add a layer

--- main.py
+++ main.py
@@ -5,37 +5,29 @@ from flask import request
 
 app = Flask(__name__, static_url_path='/', static_folder='')
 fs = []
+first_layer = []
 @app.route('/')
 def index():
-    global fs
-    fs = []
+    global fs, first_layer
+    fs = first_layer = []
+    first_layer = sorted(os.listdir("./templates"))
+    first_layer = [f for f in first_layer if f!= 'index.html']
     for root, dirs, files in os.walk("./templates", topdown=False):
         for name in files:
-            fs.append({'p':os.path.join(root, name).replace('./templates', ''), 'name':name})
-    print(fs)
-    return render_template('index.html', name='学习者', fs=fs)
+            if name != 'index.html':
+                fs.append({'p':os.path.join(root, name).replace('./templates', ''), 'name':name})
+    fs.sort(key = lambda k:k['p'])
+    return render_template('index.html', name='学习者', first_layer=first_layer)
 
 @app.before_request
 def reject():
-    if not request.path or request.path == '/':
-        return 
-    req_p_id = request.path.split('/')[1:]
+    print(request.path.split("/"))
+    if request.path == '/':
+        return
+    elif len(request.path.split("/")) == 2:
+        return render_template('index.html', name='学习者', first_layer=first_layer) #temp
     for f in fs:
-        f_id = f['p'].split(os.sep)[1:]
-        if identify(f_id, req_p_id):
-            print(f['p'][2:])
+        if f['p'].replace('\\', '/').replace(' ', '') == request.path:
             return render_template(f['p'][len(os.sep):].replace("\\", "/"))
-
-def identify(f_id, req_p_id):
-    if len(f_id) != len(req_p_id):
-        return False
-    for i, p in enumerate(req_p_id):
-        if (i != len(req_p_id) - 1 and p == f_id[i]) or \
-            (i == len(req_p_id) - 1 and f_id[i].startswith(p)):
-            continue
-        else:
-            return False
-    print(f_id, req_p_id)
-    return True
 
 app.run(debug=True, host='0.0.0.0')
